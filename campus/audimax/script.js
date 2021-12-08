13b930bd18b97a36910d90a1c838a6219d34bf4c
sewn0001 <sewn0001@stud.hs-kl.de>
2021-12-08 19:35:42 +0100

    tried to implemt doors but failed

--- campus/audimax/script.js
+++ campus/audimax/script.js
@@ -29,6 +29,39 @@ WA.onEnterZone('myZone', () => {
 });
 
 
+//let currentPopup = undefined;
+
+WA.room.onEnterZone('door_open_zone1', () => {
+    console.log("Door_Open_Zone_1");
+});
+
+WA.room.onEnterZone('clock', () => {
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes();
+    //currentPopup = WA.ui.openPopup("clockPopup","It's " + time, []);
+    WA.ui.openPopup("clockPopup","It's " + time, [{
+        label: "Close",
+        className: "normal",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+
+//WA.room.onLeaveZone('clock', closePopUp);
+
+
+
+// function closePopUp(){
+//     if (currentPopup !== undefined) {
+//         currentPopup.close();
+//         currentPopup = undefined;
+//     }
+// }
+
+
 WA.onEnterZone('HanSolozone', () => {
 WA.openPopup('HanSolo', 'Han Solo ist eine fiktive Figur aus der Star Wars- Reihe von George Lucas . Han Solo ist ein rücksichtsloser Schmuggler mit einem sarkastischen Witz und wird von  Harrison Ford gespielt.', [{
     label: "Got it!",
