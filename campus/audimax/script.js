22e077bc3fe6256cbdb87b3bbc0e54fd844e7b6f
sewn0001 <sewn0001@stud.hs-kl.de>
2021-12-08 12:48:58 +0100

    ordnerstruktur aufgebaut

---
+++ campus/audimax/script.js
@@ -0,0 +1,45 @@
+// import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+// // Calling bootstrapExtra will initiliaze all the "custom properties"  
+// bootstrapExtra();
+
+
+console.log("Hallo");
+WA.sendChatMessage('Hallo und Herzlich Willkommen auf unserem Campus :)  ', 'Easter-Eggs-Entwickler');
+
+WA.onInit().then(() => {
+    console.log('Player name: ', WA.player.name);
+})
+
+//WA.player.onPlayerMove(callback: HasPlayerMovedEventCallback): void;
+
+WA.player.onPlayerMove(console.log);
+
+
+WA.onEnterZone('myZone', () => {
+    
+    WA.openPopup('popupRectangle', 'This is an imporant message!', [{
+        label: "Got it!",
+        className: "primary",
+        callback: (popup) => {
+            
+            popup.close();
+        }
+    }]);
+});
+
+
+WA.onEnterZone('HanSolozone', () => {
+WA.openPopup('HanSolo', 'Han Solo ist eine fiktive Figur aus der Star Wars- Reihe von George Lucas . Han Solo ist ein rücksichtsloser Schmuggler mit einem sarkastischen Witz und wird von  Harrison Ford gespielt.', [{
+    label: "Got it!",
+    className: "primary",
+    callback: (popup) => {
+        
+        popup.close();
+    }
+}]);
+});
+
+
+
+
