e307ea2974be6fe2f83a1fb0d98550a085a50d56
sewn0001 <sewn0001@stud.hs-kl.de>
2021-12-15 17:25:32 +0100

    repo mit neuer Tiled map

--- campus/audimax/src/index.ts
+++ campus/audimax/src/index.ts
@@ -9,69 +9,6 @@ console.log('Hello world!');
 
 console.log("Typescript neu ");
 
-//Player movement
-WA.player.onPlayerMove(console.log);
 
-//Clock
-console.log("Bootstrap");
-let currentPopup: any = undefined;
-
-WA.room.onEnterZone('clock', () => {
-    
-    const today = new Date();
-    const time = today.getHours() + ":" + today.getMinutes();
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-
-
-
-
-WA.room.onLeaveZone('clock', closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
-
-
-//Chat Message
-
-WA.sendChatMessage('Hallo und Herzlich Willkommen auf unserem Campus :)  ', 'Easter-Eggs-Entwickler');
-
-
-//PlayerName in console
-WA.onInit().then(() => {
-    console.log('Player name: ', WA.player.name);
-})
-
-//Han Solo zone
-WA.onEnterZone('HanSolozone', () => {
-    WA.openPopup('HanSolo', 'Han Solo ist eine fiktive Figur aus der Star Wars- Reihe von George Lucas . Han Solo ist ein rücksichtsloser Schmuggler mit einem sarkastischen Witz und wird von  Harrison Ford gespielt.', [{
-        label: "Got it!",
-        className: "primary",
-        callback: (popup) => {
-            
-            popup.close();
-        }
-    }]);
-    });
-
-
-    //alien zone
-    WA.onEnterZone('alienZone', () => {
-        WA.openPopup('alien', 'Der Code Code zum Eintritt zu Capt. James T. Kirk lautet 12345', [{
-            label: "Got it!",
-            className: "primary",
-            callback: (popup) => {
-                
-                popup.close();
-            }
-        }]);
-        });
-
-
-        console.log("nachalienzone");
 
         
--- src/index.js
+++ src/index.js
@@ -1,51 +1,23 @@
+"use strict";
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
+exports.__esModule = true;
+var scripting_api_extra_1 = require("@workadventure/scripting-api-extra");
 // Calling bootstrapExtra will initiliaze all the "custom properties"  
-bootstrapExtra();
-
+(0, scripting_api_extra_1.bootstrapExtra)();
 console.log('Hello world!');
-
-
-
 // //Player movement
 // WA.player.onPlayerMove(console.log);
-
 // //Clock
 // console.log("Bootstrap");
 // let currentPopup: any = undefined;
-
 // WA.room.onEnterZone('clock', () => {
-    
 //     const today = new Date();
 //     const time = today.getHours() + ":" + today.getMinutes();
 //     currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
 // })
-
-
-
-
-
-
-
 //Chat Message
-
 WA.sendChatMessage('Hallo und Herzlich Willkommen auf unserem Campus :)  ', 'Easter-Eggs-Entwickler');
-
-
 //PlayerName in console
-WA.onInit().then(() => {
+WA.onInit().then(function () {
     console.log('Player name: ', WA.player.name);
-})
-
-
-
-
-
-        
-
-
-
-
-
+});
